7bc5f2af29edddf17878f9c050279867d7fa1f12
HuGanghui <huganghui18@gmail.com>
2020-09-30 04:53:28 -0400

    增加尝试时序数据

--- run.py
+++ run.py
@@ -9,27 +9,31 @@ def fun1(command):
 if __name__ == '__main__':
     # 多进程利用多个GPU训练模型
     process_list = []
-    data_list = ["apascal", "bank-additional-full_normalised", "lung-1vs5", "probe", "secom"]
+    data_list = ["0th_ts_train", "apascal", "bank-additional-full_normalised", "lung-1vs5", "probe", "secom"]
 
     docker_command_neither = "CUDA_VISIBLE_DEVICES={GPU} python train.py " \
                      "--data-path data/{data}.csv " \
                      "--save-path save_model_{data}/neither/ " \
-                     "--log-path logs/{data}/neither.log"  
+                     "--log-path logs/{data}/neither.log " \
+                     "--out-c 30"  
 
     docker_command_pairwise = "CUDA_VISIBLE_DEVICES={GPU} python train.py " \
                      "--data-path data/{data}.csv " \
                      "--save-path save_model_{data}/pairwise/ " \
-                     "--log-path logs/{data}/pairwise.log --use-pairwise" 
+                     "--log-path logs/{data}/pairwise.log --use-pairwise " \
+                     "--out-c 30"  
 
     docker_command_momentum = "CUDA_VISIBLE_DEVICES={GPU} python train.py " \
                     "--data-path data/{data}.csv " \
                     "--save-path save_model_{data}/momentum/ " \
-                    "--log-path logs/{data}/momentum.log --use-momentum" 
+                    "--log-path logs/{data}/momentum.log --use-momentum " \
+                     "--out-c 30"  
 
     docker_command_both = "CUDA_VISIBLE_DEVICES={GPU} python train.py " \
                     "--data-path data/{data}.csv " \
                     "--save-path save_model_{data}/both/ " \
-                    "--log-path logs/{data}/both.log --use-pairwise --use-momentum" 
+                    "--log-path logs/{data}/both.log --use-pairwise --use-momentum " \
+                    "--out-c 30"  
 
     docker_command_list = [docker_command_neither, docker_command_pairwise, 
                             docker_command_momentum, docker_command_both]                                 
@@ -38,7 +42,7 @@ if __name__ == '__main__':
     #     p.start()
     #     process_list.append(p)
     for i in range(4):
-        p = Process(target=fun1, args=(docker_command_list[i].format(GPU=4 + i, data=data_list[3]),))
+        p = Process(target=fun1, args=(docker_command_list[i].format(GPU=i, data=data_list[0]),))
         p.start()
         process_list.append(p)
     for i in process_list:
--- run_test.py
+++ run_test.py
@@ -7,7 +7,7 @@ def fun1(command):
 if __name__ == '__main__':
     # 使用对应数据集的对应模型来测试不同的方法
     process_list = []
-    data_list = ["apascal", "bank-additional-full_normalised", "lung-1vs5", "probe", "secom"]
+    data_list = ["0th_ts_train", "apascal", "bank-additional-full_normalised", "lung-1vs5", "probe", "secom"]
 
     criterion_distance = "--criterion distance"
 
@@ -35,15 +35,15 @@ if __name__ == '__main__':
                     "--load-path save_model_{data}/both/ " \
                     "--log-path test_logs/{data}/both.log "
 
-    gpu_index = 1
-    data_index = 3
-    # fun1(docker_command_neither.format(GPU=gpu_index, data=data_list[data_index]) + criterion_distance)
-    # # fun1(docker_command_neither.format(GPU=gpu_index, data=data_list[1]) + criterion_lof)
-    # fun1(docker_command_neither.format(GPU=gpu_index, data=data_list[data_index]) + criterion_iforest)
+    gpu_index = 6
+    data_index = 0
+    fun1(docker_command_neither.format(GPU=gpu_index, data=data_list[data_index]) + criterion_distance)
+    # fun1(docker_command_neither.format(GPU=gpu_index, data=data_list[1]) + criterion_lof)
+    fun1(docker_command_neither.format(GPU=gpu_index, data=data_list[data_index]) + criterion_iforest)
     
-    # fun1(docker_command_momentum.format(GPU=gpu_index, data=data_list[data_index]) + criterion_distance)
-    # # fun1(docker_command_momentum.format(GPU=gpu_index, data=data_list[1]) + criterion_lof)
-    # fun1(docker_command_momentum.format(GPU=gpu_index, data=data_list[data_index]) + criterion_iforest)
+    fun1(docker_command_momentum.format(GPU=gpu_index, data=data_list[data_index]) + criterion_distance)
+    # fun1(docker_command_momentum.format(GPU=gpu_index, data=data_list[1]) + criterion_lof)
+    fun1(docker_command_momentum.format(GPU=gpu_index, data=data_list[data_index]) + criterion_iforest)
 
     fun1(docker_command_both.format(GPU=gpu_index, data=data_list[data_index]) + criterion_distance)
     # fun1(docker_command_both.format(GPU=gpu_index, data=data_list[1]) + criterion_lof)
